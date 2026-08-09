a104624b61361ba1f7d586e3699b2ad6c55c99a2
rakibulvsc <mrakibulhasan755@gmail.com>
2026-08-09 10:34:56 +0600

    loop practice

---
+++ loop.js
@@ -0,0 +1,7 @@
+// for(let count=0;count<10;count++){
+//     console.log("do something")
+// }
+
+for(let i=0;i<10;i++){
+    console.log("value of i:",i)
+}
---
+++ while.js
@@ -0,0 +1,11 @@
+// for(let i=10; i<20;i++){
+//     console.log(i)
+// }
+
+// let count=0;
+// while(count<9){
+// console.log(count);
+// count++;
+// }
+
+
