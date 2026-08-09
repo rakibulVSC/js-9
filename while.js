c565a67f036fdc58e1304c46e8006e7c01d37a7b
rakibulvsc <mrakibulhasan755@gmail.com>
2026-08-09 13:40:09 +0600

    sum learning

--- while.js
+++ while.js
@@ -9,3 +9,15 @@
 // }
 
 
+// let sum=0;
+// for (let i=1; i<10;i++){
+//     sum=sum+ i;    
+//     console.log(i,sum)
+// }
+
+let sum=0;
+for (let i=1;i<9;i++){
+    sum=sum+i;
+    
+}
+console.log(sum)
