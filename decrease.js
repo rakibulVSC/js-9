7eaba4d7f58d43a9d99f2eb7c286aef237bc3d5e
rakibulvsc <mrakibulhasan755@gmail.com>
2026-08-09 22:40:14 +0600

    break learn

--- decrease.js
+++ decrease.js
@@ -2,7 +2,11 @@
 //     console.log(i)
 // }
 
-const number=3;
-for (let i=1;i<=10;i++){
-    console.log(number,'X',i,'=',i*number);
+// const number=3;
+// for (let i=1;i<=10;i++){
+//     console.log(number,'X',i,'=',i*number);
+// }
+
+for (let i=1;i<15;i+=2){
+    console.log(i)
 }
---
+++ practice.js
@@ -0,0 +1,20 @@
+//  let sum=0;
+// for (let i=3;i<=50;i+=3){
+//    sum=sum+i;
+//   }
+
+// console.log(sum)
+
+// let sum=0;
+// for (let i=1;i<20;i+=2){
+//    console.log(i)
+// }
+// console.log(sum)
+
+
+for (let i=0;i<=10;i++){
+    if(i === 6){
+        break;
+    }
+    console.log(i)
+}
