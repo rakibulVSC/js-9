2ab1c4b58d3327cebea24c1f349443f1ee79dba2
rakibulvsc <mrakibulhasan755@gmail.com>
2026-08-09 10:10:49 +0600

    loop start session

---
+++ start.js
@@ -0,0 +1,16 @@
+// const username = "rakib";
+// const password = "12345";
+// if(username === "rakib"){
+//     if(password === "12395"){
+//         console.log("Log in successful")
+//     }
+//     else{
+//         console.log("Wrong password")
+//     }
+// }
+// else {
+//     console.log("User not found")
+// }
+
+
+
