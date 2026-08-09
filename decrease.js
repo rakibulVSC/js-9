7949018cc7cff518efa89bf24256570759cdb320
rakibulvsc <mrakibulhasan755@gmail.com>
2026-08-09 21:05:01 +0600

    learn multiple with for loop

--- decrease.js
+++ decrease.js
@@ -1,3 +1,8 @@
-for (let i=10;i>=0;i--){
-    console.log(i)
+// for (let i=10;i>=0;i--){
+//     console.log(i)
+// }
+
+const number=3;
+for (let i=1;i<=10;i++){
+    console.log(number,'X',i,'=',i*number);
 }
